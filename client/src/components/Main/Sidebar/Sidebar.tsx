1663963acf414f24256055ece225e1d268d6c454
Skeefz <bogdanaks@bk.ru>
2020-05-18 13:51:50 +0300

    Change css sidebar

--- client/src/components/Main/Sidebar/Sidebar.tsx
+++ client/src/components/Main/Sidebar/Sidebar.tsx
@@ -1,33 +1,36 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCommentDots } from '@fortawesome/free-regular-svg-icons'
+import { faCog, faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 
 import user from '../../../assets/user.jpg'
 import './Sidebar.scss'
 
-const Sidebar = () => {
+const Sidebar: React.FC = () => {
     return (
         <div className="sidebar">
 
-            <div className="sidebar-item sidebar-item--mb-1">
+            <div className="sidebar-item">
                 <div className="sidebar__photo">
                     <a href="/"><img src={user} alt="user"/></a>
                 </div>
             </div>
 
-            <div className="sidebar-item sidebar-item--mb-1">
+            <div className="sidebar-item sidebar-item--active">
                 <div className="sidebar__chat">
-                    <a href="/"><img src={user} alt="chat"/></a>
+                    <a href="/"><FontAwesomeIcon icon={faCommentDots} /></a>
                 </div>
             </div>
 
-            <div className="sidebar-item sidebar-item--mb-1">
+            <div className="sidebar-item">
                 <div className="sidebar__settings">
-                    <a href="/"><img src={user} alt="settings"/></a>
+                    <a href="/"><FontAwesomeIcon icon={faCog} /></a>
                 </div>
             </div>
 
             <div className="sidebar-item sidebar-item--last-pos">
                 <div className="sidebar__auth">
-                    <a href="/"><img src={user} alt="auth"/></a>
+                    <a href="/"><FontAwesomeIcon icon={faSignOutAlt} /></a>
                </div>
             </div>
 
